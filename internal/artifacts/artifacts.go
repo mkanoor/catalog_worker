76d6360e83419e6ed1c287d3e5fc5f02f8b7d6e9
Madhu Kanoor <mkanoor@redhat.com>
2020-10-26 17:57:43 -0400

    Use constants

--- internal/artifacts/artifacts.go
+++ internal/artifacts/artifacts.go
@@ -2,18 +2,21 @@ package artifacts
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
+
+const ExposePrefix = "expose_to_cloud_redhat_com_"
+const MaxArtifactsBytes = 1024
 
 // Sanctify the JSON payload for artifacts. The attribute key in the artifacts
 // map should start with expose_to_cloud_redhat_com_ else they are excluded
 func Sanctify(data map[string]interface{}) (map[string]interface{}, error) {
 	result := make(map[string]interface{})
 	for k, v := range data {
-		if strings.HasPrefix(k, "expose_to_cloud_redhat_com_") {
+		if strings.HasPrefix(k, ExposePrefix) {
 			result[k] = v
 		}
 	}
@@ -24,8 +27,8 @@ func Sanctify(data map[string]interface{}) (map[string]interface{}, error) {
 		return nil, err
 	}
 
-	if len(b) > 1024 {
-		err = errors.New("Artifacts is greater than 1024 bytes")
+	if len(b) > MaxArtifactsBytes {
+		err = fmt.Errorf("Artifacts is greater than %d bytes", MaxArtifactsBytes)
 		return nil, err
 	}
 	return result, nil
